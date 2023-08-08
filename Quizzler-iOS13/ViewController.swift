7f608c9d1e6b33bd1a219a202d426725fc895a25
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2023-08-08 19:06:43 +0100

    added array of answers

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is greater than One",
+        "Three + Eight is than Ten"
+    ]
+
+    var questionNumber = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+
+        questionNumber += 1
+        updateUI()
+    }
+
+    func updateUI() {
+
+        self.questionLabel.text = quiz[questionNumber]
+    }
 
 }
 
