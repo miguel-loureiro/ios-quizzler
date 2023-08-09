755d7c53b6c6aa81ad5afd58daf7ec6c66e40c47
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2023-08-09 17:37:37 +0100

    implemented MVC pattern on default choice quiz

--- Quizzler-iOS13/Model/QuizLogic.swift
+++ Quizzler-iOS13/Model/QuizLogic.swift
@@ -1,20 +1,15 @@
 //
-//  ViewController.swift
+//  QuizLogic.swift
 //  Quizzler-iOS13
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by António Loureiro on 09/08/2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+struct QuizLogic {
     
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
-
     let quiz = [
         Question(q: "A slug's blood is green.", a: "True"),
         Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
@@ -31,36 +26,41 @@ class ViewController: UIViewController {
     ]
 
     var questionNumber = 0
+    var score = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        updateUI()
+    mutating func checkAnswer(_ userAnswer: String)  -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            score += 1
+            return true
+        } else {
+            return false
+        }
     }
 
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
 
-        let userAnswer = sender.currentTitle //True, False
-        let actualAnswer = quiz[questionNumber].answer
-        if userAnswer == actualAnswer {
-            print("Right!")
-        } else {
-            print("Wrong!")
-        }
+    func getProgress() -> Float {
+        let progress = Float(questionNumber + 1) / Float(quiz.count)
+        return progress
+    }
 
+    mutating func nextQuestion() {
         if questionNumber + 1 < quiz.count {
             questionNumber += 1
         } else {
+            
             questionNumber = 0
+            score = 0
         }
-
-        updateUI()
     }
 
-    func updateUI() {
-
-        self.questionLabel.text = quiz[questionNumber].text
+    func getScore() -> Int {
+        return score
     }
 
+    func getMaxPossibleScore() -> Int {
+        return quiz.count
+    }
 }
-
